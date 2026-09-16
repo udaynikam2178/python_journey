00e5dbccde75f424c46567ad8c3b462500c3bc2d
Uday Nikam <udaynikam2178@gmail.com>
2026-09-16 23:32:09 +0530

    very first commit to test

---
+++ basic_python.py
@@ -0,0 +1,27 @@
+# a = 10
+# b = 11
+# print(a+b)
+
+#if 5 > 3 :
+#    print("five is greater than three")
+
+#x ="5" 
+#y = "good morning"
+
+#print(x+y)
+
+#print("hello bhai"); print("how are you")
+
+
+#print('it will also work')
+#fruits = [" apple " , " banana " , " cherry "]
+#x, y, z = fruits
+#print(y)
+
+#x = "uday"
+#y = "nikam"
+#print(x , y)
+
+x = "uday"
+y = "nikam"
+print(x + y)
