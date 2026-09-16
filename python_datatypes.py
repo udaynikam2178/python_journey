6bd80fd29b321254b39d61dddea1295bba5080ab
Uday Nikam <udaynikam2178@gmail.com>
2026-09-17 00:04:00 +0530

    I am learning datatypes

---
+++ python_datatypes.py
@@ -0,0 +1,27 @@
+# integer datatype
+x = 12 
+y = 13 
+
+# string datatype
+x = "uday" 
+y = "nikam" 
+
+# list datatype 
+x = [1,2,3]
+y = [4,5,6] 
+
+#tuple datatype
+x = (1,2,3)
+y = (4,5,6)
+
+#dict datatype
+cars = {"name": "Porshe", "price":12}
+
+
+
+
+
+
+
+
+
